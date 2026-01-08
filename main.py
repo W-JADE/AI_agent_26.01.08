b9ba6c240afac3341f1536729a7978150e787808
w-JADE <threelemon@naver.com>
2026-01-08 17:55:24 +0900

    next commit

--- main.py
+++ main.py
@@ -1,40 +1,36 @@
-from fastapi import *
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# public 폴더를 정적 파일 경로로 등록(mount)
 app.mount("/public", StaticFiles(directory="public", html=True), name="public")
 
 @app.get("/")
-def root(message: str = "hello") :
+def root(message: str = "초등학생용 사칙연산 계산기") :
+    print("GET - / 요청 받았다!")
     return {
         "message": message,
-        "message length": len(message)
     }
 
+# 서버에서 사칙연산 계산기 구현해보기
+# path parameter - /연산자/{}/{}(항1/항2) 
+# 우아한 URL
+@app.get("/plus/{a}/{b}")
+def plus(a:int,b:int):
+    print("더하기 결과확인 =>",a, b, a+b)
+    return a + b
 
-# BasesModel을 상속받아서 Message클래스 선언
-from pydantic import BaseModel
+@app.get("/min/{a}/{b}")
+def plus(a:int,b:int):
+    print("빼기 결과확인 =>", a, b, a-b)
+    return a - b
 
-# 클라이언트에서 Ajax로 통신할때 JSON 데이터
-# POST로 통신 할때는 JSON으로 데이터 전송
-class Message(BaseModel):
-    text: str
+@app.get("/mult/{a}/{b}")
+def plus(a:int,b:int):
+    print("곱하기 결과확인 =>",a, b, a*b)
+    return a * b
 
-# post 메서드를 추가하고 postman으로 테스트 하기
-# Content-Type: raw로 설정 후 JSON 데이터 전송
-@app.post("/")
-def root_post(message: Message) :
-    return {
-        "message": message.text,
-        "message length": len(message.text)
-    }
-
-
-# Form 파라미터 전달 받기
-@app.post("/login")
-def login_post(username: str = Form(...)):
-    return {
-        "username": username
-    }
+@app.get("/div/{a}/{b}")
+def plus(a:int,b:int):
+    print("나누기 결과확인 =>",a, b, a/b)
+    return a / b
